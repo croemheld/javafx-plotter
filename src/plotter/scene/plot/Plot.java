fbfd236e17d4e4139b743fb20172110832dc5899
Christopher Roemheld <chris.roemheld@tum.de>
2017-11-09 23:07:20 +0100

    Added coordinate grid

--- src/plotter/scene/plot/Plot.java
+++ src/plotter/scene/plot/Plot.java
@@ -9,18 +9,19 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
-import javafx.scene.shape.Rectangle;
 import plotter.expressions.ComplexNumber;
 import plotter.expressions.Expression;
 import plotter.scene.axes.Axes;
 
 public class Plot extends Pane {
 
+	private Path gridPath;
+	private Path originPath;
 	private Axes axes;
 	private Path realPath;
 	private Path imaginaryPath;
 	private Expression expression;
-	private Tooltip tooltip = new Tooltip();
+	private Tooltip tooltip;
 	private boolean isTooltipActive = false;
 
 	public Plot(Axes axes, Expression expression) {
@@ -39,32 +40,44 @@ public class Plot extends Pane {
 			return;
 		}
 
+		gridPath = new Path();
+		originPath = new Path();
 		realPath = new Path();
 		imaginaryPath = new Path();
+		tooltip = new Tooltip();
 
-		double lowerBound = axes.getHorizontalLowerBound();
-		double upperBound = axes.getHorizontalUpperBound();
+		double xLowerBound = axes.getHorizontalLowerBound();
+		double xUpperBound = axes.getHorizontalUpperBound();
+		
+		plotGrid(axes);
 
-		boolean realOnly = plotLine(realPath, lowerBound, upperBound, true);
+		boolean realOnly = plotLine(realPath, xLowerBound, xUpperBound, true);
 
 		if (!realOnly) {
-			plotLine(imaginaryPath, lowerBound, upperBound, false);
+			plotLine(imaginaryPath, xLowerBound, xUpperBound, false);
 		}
 
 		setMinSize(Pane.USE_PREF_SIZE, Pane.USE_PREF_SIZE);
 		setPrefSize(axes.getPrefWidth(), axes.getPrefHeight());
 		setMaxSize(Pane.USE_PREF_SIZE, Pane.USE_PREF_SIZE);
 
-		getChildren().setAll(axes, realPath, imaginaryPath);
+		getChildren().setAll(gridPath, originPath, axes, realPath, imaginaryPath);
 	}
 
 	public void redraw() {
-		getChildren().setAll(axes, realPath, imaginaryPath);
+		getChildren().setAll(gridPath, originPath, axes, realPath, imaginaryPath);
+	}
+	
+	private void plotGrid(Axes axes) {
+		PlotUtil.setStroke(axes, gridPath, Color.LIGHTGRAY, 1);
+		PlotUtil.setStroke(axes, originPath, Color.DARKGRAY, 2);
+		PlotUtil.drawHorizontalLines(gridPath, originPath, axes);
+		PlotUtil.drawVerticalLines(gridPath, originPath, axes);
 	}
 
 	private boolean plotLine(Path path, double lowerBound, double upperBound, boolean isReal) {
 		boolean realOnly = true;
-		setStroke(path, (isReal ? Color.ORANGE : Color.DEEPSKYBLUE));
+		PlotUtil.setStroke(axes, path, (isReal ? Color.ORANGE : Color.DEEPSKYBLUE), 1);
 
 		for (double x = lowerBound; x < upperBound; x += PlotUtil.X_STEPS) {
 			ComplexNumber w = expression.eval(x);
@@ -73,34 +86,13 @@ public class Plot extends Pane {
 			realOnly = realOnly && w.isReal();
 
 			if (x == lowerBound) {
-				path.getElements().add(new MoveTo(mapHorizontal(x), mapVertical(y)));
+				path.getElements().add(new MoveTo(PlotUtil.mapHorizontal(axes, x), PlotUtil.mapVertical(axes, y)));
 			} else {
-				path.getElements().add(new LineTo(mapHorizontal(x), mapVertical(y)));
+				path.getElements().add(new LineTo(PlotUtil.mapHorizontal(axes, x), PlotUtil.mapVertical(axes, y)));
 			}
 		}
 
 		return realOnly;
-	}
-
-	private void setStroke(Path path, Color color) {
-		Rectangle rectangle = new Rectangle(0, 0, axes.getPrefWidth(), axes.getPrefHeight());
-		path.setStroke(color.deriveColor(0, 1, 1, 1));
-		path.setStrokeWidth(1);
-		path.setClip(rectangle);
-	}
-	
-	private double mapHorizontal(double x) {
-		double ppwu = axes.getPrefWidth() / axes.getHorizontalBound();
-		double origin = -axes.getHorizontalLowerBound() * ppwu;
-		
-		return x * ppwu + origin;
-	}
-	
-	private double mapVertical(double y) {
-		double pphu = axes.getPrefHeight() / axes.getVerticalBound();
-		double origin = axes.getVerticalUpperBound() * pphu;
-		
-		return -y * pphu + origin;
 	}
 
 	public Expression getExpression() {
@@ -143,7 +135,7 @@ public class Plot extends Pane {
 		tooltip.setText("x = " + x + ", f(x) = " + y);
 		
 		// TODO: instead of 150px calculate the height difference from window top border to beginning of axes
-		tooltip.show((Node) event.getSource(), event.getSceneX(), mapVertical(expression.eval(x).getReal()) + 150);
+		tooltip.show((Node) event.getSource(), event.getSceneX(), PlotUtil.mapVertical(axes, expression.eval(x).getReal()) + 150);
 	}
 
 	public void hideCoordinates() {
--- src/plotter/scene/plot/PlotUtil.java
+++ src/plotter/scene/plot/PlotUtil.java
@@ -1,6 +1,12 @@
 package plotter.scene.plot;
 
 import javafx.geometry.Insets;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.Rectangle;
+import plotter.scene.axes.Axes;
 
 public class PlotUtil {
 
@@ -21,6 +27,74 @@ public class PlotUtil {
 
 	private PlotUtil() {
 		// No instantiation
+	}
+	
+	public static void drawHorizontalLines(Path gridPath, Path originPath, Axes axes) {
+		double xLowerBound = axes.getHorizontalLowerBound();
+		double xUpperBound = axes.getHorizontalUpperBound();
+		double yLowerBound = axes.getVerticalLowerBound();
+		double yUpperBound = axes.getVerticalUpperBound();
+		double yUnit = axes.getVerticalAxis().getTickUnit();
+		
+		for(double y = yLowerBound + yUnit; y <= yUpperBound; y += yUnit) {
+			if(y == 0) {
+				addLine(originPath, axes, xLowerBound, y, xUpperBound, y);
+				continue;
+			}
+			
+			addLine(gridPath, axes, xLowerBound, y, xUpperBound, y);
+		}
+	}
+	
+	public static void drawVerticalLines(Path gridPath, Path originPath, Axes axes) {
+		double xLowerBound = axes.getHorizontalLowerBound();
+		double xUpperBound = axes.getHorizontalUpperBound();
+		double yLowerBound = axes.getVerticalLowerBound();
+		double yUpperBound = axes.getVerticalUpperBound();
+		double xUnit = axes.getHorizontalAxis().getTickUnit();
+		
+		for(double x = xLowerBound + xUnit; x <= xUpperBound; x += xUnit) {
+			if(x == 0) {
+				addLine(originPath, axes, x, yLowerBound, x, yUpperBound);
+				continue;
+			}
+			
+			addLine(gridPath, axes, x, yLowerBound, x, yUpperBound);
+		}
+	}
+	
+	private static void addLine(Path path, Axes axes, double xFrom, double yFrom, double xTo, double yTo) {
+		path.getElements().add(new MoveTo(mapHorizontal(axes, xFrom), mapVertical(axes, yFrom)));
+		path.getElements().add(new LineTo(mapHorizontal(axes, xTo), mapVertical(axes, yTo)));
+	}
+	
+	public static double pixelsPerWidthUnit(Axes axes) {
+		return axes.getPrefWidth() / axes.getHorizontalBound();
+	}
+	
+	public static double pixelsPerHeightUnit(Axes axes) {
+		return axes.getPrefHeight() / axes.getVerticalBound();
+	}
+
+	public static void setStroke(Axes axes, Path path, Color color, int width) {
+		Rectangle rectangle = new Rectangle(0, 0, axes.getPrefWidth(), axes.getPrefHeight());
+		path.setStroke(color.deriveColor(0, 1, 1, 1));
+		path.setStrokeWidth(width);
+		path.setClip(rectangle);
+	}
+	
+	public static double mapHorizontal(Axes axes, double x) {
+		double ppwu = pixelsPerWidthUnit(axes);
+		double origin = -axes.getHorizontalLowerBound() * ppwu;
+		
+		return x * ppwu + origin;
+	}
+	
+	public static double mapVertical(Axes axes, double y) {
+		double pphu = pixelsPerHeightUnit(axes);
+		double origin = axes.getVerticalUpperBound() * pphu;
+		
+		return -y * pphu + origin;
 	}
 
 }
